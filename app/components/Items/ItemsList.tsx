44cdf5a9f5803158e03241767c37f700ca790734
pavelxw <nahid.pavel@xen.works>
2024-03-18 22:08:59 +0600

    fix ts error

--- app/components/Items/ItemsList.tsx
+++ app/components/Items/ItemsList.tsx
@@ -40,64 +40,72 @@ const ItemsList = ({ items }) => {
       </h4>
       {/* <button className="bg-green-300 py-2 px-3" onClick={()=>{}}>Pin to Home</button> */}
 
-      {Object.entries(groupedItems)?.map(([date, itemList]) => (
-        <div key={date}>
-          <h3>Date: {date}</h3>
+      {Object.entries(groupedItems as Record<string, any[]>)?.map(
+        ([date, itemList]) => (
+          <div key={date}>
+            <h3>Date: {date}</h3>
 
-          <table className="w-full border border-black my-3">
-            <thead>
-              <tr>
-                <th className="border border-black bg-slate-400">Date</th>
-                <th className="border border-black bg-slate-400">Item Name</th>
-                <th className="border border-black text-center bg-slate-400">
-                  Price
-                </th>
-                <th className="border border-black bg-slate-400">Quantity</th>
-                <th className="border border-black bg-slate-400">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {itemList.map(({ name, amount, quantity, _id }) => (
-                <tr key={_id}>
-                  <td className="border border-black">{date}</td>
-                  <td className="border border-black">{name}</td>
-                  <td className="border border-black text-center">{amount}</td>
-                  <td className="border border-black text-center">
-                    {quantity}
-                  </td>
-                  <td className="border border-black  ">
-                    <span className="flex gap-6 justify-center">
-                      <MdEdit
-                        onClick={() => {
-                          router.push(`/editItem/${_id}`);
-                        }}
-                      />
-
-                      <CiCircleRemove
-                        onClick={() => {
-                          removeHandler(_id);
-                        }}
-                      />
-                    </span>
-                  </td>
+            <table className="w-full border border-black my-3">
+              <thead>
+                <tr>
+                  <th className="border border-black bg-slate-400">Date</th>
+                  <th className="border border-black bg-slate-400">
+                    Item Name
+                  </th>
+                  <th className="border border-black text-center bg-slate-400">
+                    Price
+                  </th>
+                  <th className="border border-black bg-slate-400">Quantity</th>
+                  <th className="border border-black bg-slate-400">Actions</th>
                 </tr>
-              ))}
-            </tbody>
-            <tfoot className="bg-slate-200">
-              <td />
-              <td className="text-center">
-                <dt>Total</dt>
-              </td>
+              </thead>
+              <tbody>
+                {itemList.map(({ name, amount, quantity, _id }) => (
+                  <tr key={_id}>
+                    <td className="border border-black">{date}</td>
+                    <td className="border border-black">{name}</td>
+                    <td className="border border-black text-center">
+                      {amount}
+                    </td>
+                    <td className="border border-black text-center">
+                      {quantity}
+                    </td>
+                    <td className="border border-black  ">
+                      <span className="flex gap-6 justify-center">
+                        <MdEdit
+                          onClick={() => {
+                            router.push(`/editItem/${_id}`);
+                          }}
+                        />
 
-              <td className="text-center">
-                <dt>{itemList.reduce((sum, item) => sum + item.amount, 0)}</dt>
-              </td>
-              <td />
-              <td />
-            </tfoot>
-          </table>
-        </div>
-      ))}
+                        <CiCircleRemove
+                          onClick={() => {
+                            removeHandler(_id);
+                          }}
+                        />
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot className="bg-slate-200">
+                <td />
+                <td className="text-center">
+                  <dt>Total</dt>
+                </td>
+
+                <td className="text-center">
+                  <dt>
+                    {itemList.reduce((sum, item) => sum + item.amount, 0)}
+                  </dt>
+                </td>
+                <td />
+                <td />
+              </tfoot>
+            </table>
+          </div>
+        )
+      )}
     </>
   );
 };
